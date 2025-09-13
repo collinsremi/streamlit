465d183941dac9b197dc2a58f0ca46a29430aa65
Diugwu Izuchukwu <izudiugwu@gmail.com>
2025-09-13 04:01:42 +0100

    testing ci/cd pipline

--- practice codes/iris_app_code.py
+++ practice codes/iris_app_code.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
 import joblib 
 
 st.title("My model app")
 st.write("this site gives prediction to Iris species")
-st.header("Please input your features")
+
 
 st.sidebar.header("Input Features")
 st.feature1 = st.sidebar.slider("Sepal Length (cm)", 4.0, 8.0, 5.8)
@@ -14,9 +13,8 @@ st.feature3 = st.sidebar.slider("Petal Length (cm)", 1.0, 7.0, 4.35)
 st.feature4 = st.sidebar.slider("Petal Width (cm)", 0.1, 2.5, 1.3)
 
 st.write(st.feature1, st.feature2, st.feature3, st.feature4)
-st.write(st.feature1 + st.feature2 + st.feature3 + st.feature4)
 
-model = joblib.load(r"C:\Users\Admin\Documents\Datascience\practice codes\model")
+model = joblib.load(r"streamlit\practice codes\model.pkl")
 
 dict_data = {
     0: "Setosa",
